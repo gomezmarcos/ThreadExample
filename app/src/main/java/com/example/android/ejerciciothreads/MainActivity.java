453ddb97903c34ce9551f90013af1a2a55026d53
marcos-utn <elcorreodemarcos@gmail.com>
2015-10-15 05:16:45 -0300

    ahora se puede modificar las view - usando Handler, Message y Callbak

--- app/src/main/java/com/example/android/ejerciciothreads/MainActivity.java
+++ app/src/main/java/com/example/android/ejerciciothreads/MainActivity.java
@@ -1,32 +1,28 @@
 package com.example.android.ejerciciothreads;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Handler.Callback{
     Thread thread;
-
+    Handler handler = new Handler(this);
+    TextView txt ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread thread = new MyThread();
+        txt = (TextView) findViewById(R.id.text);
+
+        Thread thread = new MyThread(this.handler);
         thread.start();
 
-        int i = 0;
-        while (true){
-            try {
-                Thread.sleep(3000l);
-                Log.e("MainAcitivity", String.valueOf(i++));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return; //para forzar que se salga del while
-            }
-        }
     }
 
     @Override
@@ -36,5 +32,13 @@ public class MainActivity extends AppCompatActivity {
         if (thread.isAlive()){
             thread.interrupt();
         }
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        //txt.setText(String.valueOf(msg.obj));
+        TextView txt1 = (TextView) findViewById(R.id.text);
+        txt1.setText(String.valueOf(msg.obj));
+        return true;
     }
 }
--- app/src/main/java/com/example/android/ejerciciothreads/MyThread.java
+++ app/src/main/java/com/example/android/ejerciciothreads/MyThread.java
@@ -1,17 +1,34 @@
 package com.example.android.ejerciciothreads;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.UiThread;
 import android.util.Log;
+import android.widget.TextView;
 
 /**
  * Created by android on 08/10/15.
  */
 public class MyThread extends Thread{
+
+    private final Handler handler;
+
+    public MyThread(Handler handler){
+        this.handler = handler;
+    }
+
     @Override
     public void run() {
-        while (true){
+        for (int i = 0; i<10; i++){
             try {
-                sleep(6000l);
+                sleep(2000l);
                 Log.d("MyThread", "escribiendo..");
+
+                Message message = new Message();
+                message.obj = "modificado desde el thread "+String.valueOf(i);
+                handler.sendMessage(message);
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
